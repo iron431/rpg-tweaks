470a516448cdc93fa4dd98d61be28d7275e28327
Christian <cmb@bitfly.org>
2023-06-12 14:43:28 -0500

    Remove EnchDesc mixin, generify desciption remover

--- src/main/java/io/redspace/ironsrpgtweaks/enchantment_module/EnchantmentClientEvents.java
+++ src/main/java/io/redspace/ironsrpgtweaks/enchantment_module/EnchantmentClientEvents.java
@@ -1,20 +1,25 @@
 package io.redspace.ironsrpgtweaks.enchantment_module;
 
+import io.redspace.ironsrpgtweaks.IronsRpgTweaks;
 import io.redspace.ironsrpgtweaks.config.ServerConfigs;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -25,23 +30,31 @@ public class EnchantmentClientEvents {
 
     @SubscribeEvent
     public static void modifyTooltip(ItemTooltipEvent event) {
-        //TODO... client can just disable it :skull:
         if (!ServerConfigs.ENCHANT_MODULE_ENABLED.get())
             return;
         ItemStack stack = event.getItemStack();
         ListTag enchants = EnchantHelper.getEnchantments(stack);
         var tooltipComponents = event.getToolTip();
-        //TODO: checks for performance. Mixin would probably be way more performant
         if (enchants != null) {
             if (EnchantHelper.shouldHideEnchantments(stack))
-                for (int i = 0; i < enchants.size(); i++) {
-                    CompoundTag compoundtag = enchants.getCompound(i);
-                    Registry.ENCHANTMENT.getOptional(EnchantmentHelper.getEnchantmentId(compoundtag)).ifPresent((enchant) -> {
-                        var component = enchant.getFullname(EnchantmentHelper.getEnchantmentLevel(compoundtag));
-                        int j = tooltipComponents.indexOf(component);
-                        if (j >= 0)
-                            ((MutableComponent) tooltipComponents.get(j)).setStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withFont(ENCHANT_FONT));
-                    });
+                for (int e = 0; e < enchants.size(); e++) {
+                    CompoundTag compoundtag = enchants.getCompound(e);
+                    Enchantment enchantment = ForgeRegistries.ENCHANTMENTS.getValue(EnchantmentHelper.getEnchantmentId(compoundtag));
+                    if (enchantment != null) {
+                        var enchantKey = enchantment.getDescriptionId();
+                        for (int i = tooltipComponents.size() - 1; i >= 0; i--) {
+                            if (tooltipComponents.get(i).getContents() instanceof TranslatableContents translatableContents) {
+                                var tooltipKey = translatableContents.getKey();
+                                if (tooltipKey.equals(enchantKey)) {
+                                    //This is the enchantment, hide it
+                                    ((MutableComponent) tooltipComponents.get(i)).setStyle(Style.EMPTY.withColor(ChatFormatting.GRAY).withFont(ENCHANT_FONT));
+                                } else if (tooltipKey.contains(enchantKey)) {
+                                    //this is something dependent on the enchantment, such as a description. remove it.
+                                    tooltipComponents.remove(i);
+                                }
+                            }
+                        }
+                    }
                 }
         }
     }
